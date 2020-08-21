a68da740b7d2eb93d34992d6170fb7fb55e325f9
Hitesh Sharma <hitesharma3899@gmail.com>
2020-08-21 11:24:00 +0530

    extracted resuable form component from loginForm

---
+++ src/components/common/form.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./input";
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {},
+  };
+
+  validate = () => {
+    const option = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, option);
+    if (!error) return null;
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({ id, value }) => {
+    const obj = { [id]: value };
+    const schema = { [id]: this.schema[id] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    this.doSubmit();
+  };
+
+  changeHandler = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.id] = errorMessage;
+    else delete errors[input.id];
+
+    const data = { ...this.state.data };
+    data[input.id] = input.value;
+    this.setState({ data, errors });
+  };
+
+  renderInput(id, label) {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        id={id}
+        value={data[id]}
+        label={label}
+        onChange={this.changeHandler}
+        error={errors[id]}
+      />
+    );
+  }
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+}
+
+export default Form;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
-import Input from "./common/input";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {},
   };
 
@@ -13,61 +13,18 @@ class LoginForm extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const option = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, option);
-    if (!error) return null;
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  validateProperty = ({ id, value }) => {
-    const obj = { [id]: value };
-    const schema = { [id]: this.schema[id] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  submitHandler = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-  };
-
-  changeHandler = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.id] = errorMessage;
-    else delete errors[input.id];
-
-    const account = { ...this.state.account };
-    account[input.id] = input.value;
-    this.setState({ account, errors });
-  };
+  doSubmit = () => {
+    console.log('Submitted');
+  }
 
   render() {
-    const { account, errors } = this.state;
     return (
       <React.Fragment>
+        <h1>Login</h1>
         <form onSubmit={this.submitHandler}>
-          <Input
-            id="username"
-            value={account.username}
-            label="Username"
-            onChange={this.changeHandler}
-            error={errors.username}
-          />
-          <Input
-            id="password"
-            value={account.password}
-            label="Password"
-            onChange={this.changeHandler}
-            error={errors.password}
-          />
-          <button className="btn btn-primary">Login</button>
+          {this.renderInput('username', 'Username')}
+          {this.renderInput('password', 'Password')}
+          {this.renderButton('Login')}
         </form>
       </React.Fragment>
     );
